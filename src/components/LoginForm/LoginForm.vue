743f498d692640b4d9fd96ef395400ae8b142896
Arthur Penteado <ascpenteado@gmail.com>
2024-02-05 03:47:23 -0300

    feat: add login functionality to LoginForm.vue

--- src/components/LoginForm/LoginForm.vue
+++ src/components/LoginForm/LoginForm.vue
@@ -19,17 +19,32 @@
 <script lang="ts" setup>
 import { ref } from "vue";
 import { showSnackbar } from "../../store/snackBar/snackBar.state";
+import { apiClient } from "../../services/api.service";
+import { ObjetoUsuario } from "../../types/api.types";
+import router from "../../router";
 
 const email = ref("");
 const password = ref("");
 
-const login = () => {
+const login = async () => {
   if (!email.value) {
     return showSnackbar("O email é obrigatório", "red");
   }
 
   if (!password.value) {
     return showSnackbar("A senha é obrigatória", "red");
+  }
+
+  const res = await apiClient.post<ObjetoUsuario>("/login", {
+    email: email.value,
+    senha: password.value,
+  });
+
+  if (!res) return;
+
+  if (res.token) {
+    localStorage.setItem("token", res.token);
+    router.push("/");
   }
 };
 </script>
