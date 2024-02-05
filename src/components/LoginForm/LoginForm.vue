1cef52fd1288a728db353d8154bb858c81e061dc
Arthur Penteado <ascpenteado@gmail.com>
2024-02-05 02:50:46 -0300

    feat: add LoginForm and LoginView components

---
+++ src/components/LoginForm/LoginForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-form @submit.prevent="login">
+    <v-text-field
+      v-model="email"
+      label="Email"
+      type="email"
+      color="accent"
+    ></v-text-field>
+    <v-text-field
+      v-model="password"
+      label="Senha"
+      type="password"
+      color="accent"
+    ></v-text-field>
+    <v-btn type="submit" color="primary">Login</v-btn>
+  </v-form>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import { showSnackbar } from "../../store/snackBar/snackBar.state";
+
+const email = ref("");
+const password = ref("");
+
+const login = () => {
+  if (!email.value) {
+    return showSnackbar("O email é obrigatório", "red");
+  }
+
+  if (!password.value) {
+    return showSnackbar("A senha é obrigatória", "red");
+  }
+};
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-app app>
+      <v-main class="d-flex justify-content-center align-center">
+        <v-container class="d-flex flex-grow-1 justify-center align-center">
+          <v-card width="100%" max-width="550">
+            <div class="primary pa-4 d-flex justify-center">
+              <div class="d-flex white--text text-xs-h1 align-center">
+                <v-icon color="secondary" class="text-h4">mdi-poll</v-icon>
+                <span class="white--text ml-5 text-h4">AlterDash</span>
+              </div>
+            </div>
+            <v-card-title>Login</v-card-title>
+            <v-card-text>
+              <LoginForm />
+            </v-card-text>
+          </v-card>
+        </v-container>
+        <snack-bar />
+      </v-main>
+    </v-app>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import LoginForm from "@/components/LoginForm/LoginForm.vue";
+import SnackBar from "@/components/SnackBar/SnackBar.vue";
+</script>
