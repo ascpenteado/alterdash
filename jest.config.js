8451cad398314a50881a01f33e5945f38b218933
Arthur Penteado <ascpenteado@gmail.com>
2024-02-06 15:40:04 -0300

    chore: add testMatch and transformIgnorePatterns to jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  testMatch: ["<rootDir>/src/**/*.spec.(js|jsx|ts|tsx)"],
+  transformIgnorePatterns: ["/node_modules/(?!(vuetify|axios)/)"],
+  verbose: true,
 };
