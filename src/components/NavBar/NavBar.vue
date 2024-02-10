025de81e6e48b180cbb9e54a3e785e9719982f9e
Arthur Penteado <ascpenteado@gmail.com>
2024-02-10 14:15:24 -0300

    feat: refactor NavBar and LoginView components with new logo

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -1,16 +1,22 @@
 <template>
   <v-app-bar app color="primary">
-    <div class="d-flex align-center justify-space-between flex-grow-1">
+    <div
+      class="d-flex align-center justify-space-between flex-grow-1 flex-shrink-0"
+    >
       <router-link to="/">
-        <div class="d-flex white--text text-xs-h1 align-center">
-          <v-icon color="secondary" class="text-h4">mdi-poll</v-icon>
-          <span class="white--text ml-5 text-h4">AlterDash</span>
+        <div class="d-flex white--text align-center pa-2 logo-wrapper">
+          <app-logo color="white"></app-logo>
         </div>
       </router-link>
 
-      <nav class="d-none d-sm-flex align-center">
-        <v-btn text to="/products" color="secondary">Produtos</v-btn>
-        <v-btn text to="/clients" color="secondary">Clientes</v-btn>
+      <nav class="d-none d-sm-flex align-center nav">
+        <v-btn text to="/" class="nav-item" color="secondary">Painel</v-btn>
+        <v-btn text to="/products" class="nav-item" color="secondary"
+          >Produtos</v-btn
+        >
+        <v-btn text to="/clients" class="nav-item" color="secondary"
+          >Clientes</v-btn
+        >
         <nav-drop />
       </nav>
     </div>
@@ -26,4 +32,25 @@
 import { RouterLink } from "vue-router";
 import NavDrop from "../NavDropdown/NavDropdown.vue";
 import { mobileNavActions } from "../../store/mobileNav/mobileNav.state";
+import AppLogo from "../AppLogo/AppLogo.vue";
 </script>
+
+<style scoped lang="scss">
+.nav {
+  gap: 0.5rem;
+  .nav-item {
+    padding: 0.5rem;
+    font-size: 0.75rem;
+    @media screen and (min-width: 1200px) {
+      font-size: 1rem;
+      padding: 1rem;
+    }
+  }
+  .v-btn--active {
+  }
+}
+
+.logo-wrapper {
+  max-width: 200px;
+}
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,10 +4,9 @@
       <v-main class="d-flex justify-content-center align-center">
         <v-container class="d-flex flex-grow-1 justify-center align-center">
           <v-card width="100%" max-width="550">
-            <div class="primary pa-4 d-flex justify-center">
-              <div class="d-flex white--text text-xs-h1 align-center">
-                <v-icon color="secondary" class="text-h4">mdi-poll</v-icon>
-                <span class="white--text ml-5 text-h4">AlterDash</span>
+            <div class="primary d-flex justify-center">
+              <div class="d-flex align-center logo-wrapper pa-4">
+                <app-logo color="white"></app-logo>
               </div>
             </div>
             <v-card-title>Login</v-card-title>
@@ -26,13 +25,21 @@
 import Vue from "vue";
 import LoginForm from "@/components/LoginForm/LoginForm.vue";
 import SnackBar from "@/components/SnackBar/SnackBar.vue";
+import AppLogo from "../components/AppLogo/AppLogo.vue";
 
 const LoginView = Vue.extend({
   components: {
     LoginForm,
     SnackBar,
+    AppLogo,
   },
 });
 
 export default LoginView;
 </script>
+
+<style lang="scss">
+.logo-wrapper {
+  max-width: 80%;
+}
+</style>
