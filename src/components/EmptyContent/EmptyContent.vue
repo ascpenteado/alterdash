8c1349e5bf4ccec14b831aefc439c5634ff6ca90
Arthur Penteado <ascpenteado@gmail.com>
2024-02-11 16:26:07 -0300

    feat: add EmptyContent component

---
+++ src/components/EmptyContent/EmptyContent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="no-data">
+    <img
+      class="illustration"
+      src="../../assets/svg/empty.svg"
+      alt="Nenhum dado encontrado"
+    />
+    <p class="text-h3 mt-10">Nenhum registro por aqui...</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.no-data {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  padding: 2rem;
+
+  .illustration {
+    max-width: 230px;
+    @media screen and (min-width: 600px) {
+      max-width: 330px;
+    }
+  }
+}
+</style>
