3ba00d997a7aa1de8bbe7ee55c20fb0d082cf1cf
Arthur Penteado <ascpenteado@gmail.com>
2024-02-11 10:57:49 -0300

    refactor: update import paths and update storage usage

--- src/__tests__/unit/logout/logout.spec.ts
+++ src/__tests__/unit/logout/logout.spec.ts
@@ -1,23 +1,23 @@
-import { logout } from "../../../services/logout";
-import router from "../../../router";
-import { useLocalStorageMock } from "../../../utils/useLocalStorageMock";
+import { logout } from "@/services/logout";
+import router from "@/router";
+import { useSessionStorareMock } from "@/utils/useSessionStorageMock";
 
-useLocalStorageMock();
+useSessionStorareMock();
 jest.mock("../../../router");
 
 describe("logout", () => {
   beforeEach(() => {
     jest.clearAllMocks();
-    localStorage.clear();
+    sessionStorage.clear();
   });
 
-  it("should remove token from localStorage and redirect to '/login' if there is a token", async () => {
-    localStorage.setItem("token", "mockedToken");
+  it("should remove token from sessionStorage and redirect to '/login' if there is a token", async () => {
+    sessionStorage.setItem("token", "mockedToken");
     const pushSpy = jest.spyOn(router, "push");
 
     await logout();
 
-    expect(localStorage.removeItem).toHaveBeenCalledWith("token");
+    expect(sessionStorage.removeItem).toHaveBeenCalledWith("token");
     expect(pushSpy).toHaveBeenCalledWith("/login");
   });
 });
