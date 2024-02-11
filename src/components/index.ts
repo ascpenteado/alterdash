b4cdfbfd8137a19bd7bd2f95a954d82c1698d68f
Arthur Penteado <ascpenteado@gmail.com>
2024-02-11 16:26:26 -0300

    feat: add index for components

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+export { default as AppLogo } from "./AppLogo/AppLogo.vue";
+export { default as CrudTable } from "./CrudTable/CrudTable.vue";
+export { default as EmptyContent } from "./EmptyContent/EmptyContent.vue";
+export { default as LoginForm } from "./LoginForm/LoginForm.vue";
+export { default as NavBar } from "./NavBar/NavBar.vue";
+export { default as NavDrawer } from "./NavDrawer/NavDrawer.vue";
+export { default as NavDropdown } from "./NavDropdown/NavDropdown.vue";
+export { default as ProductForm } from "./ProductForm/ProductForm.vue";
+export { default as SnackBar } from "./SnackBar/SnackBar.vue";
+export { default as ViewToolbar } from "./ViewToolbar/ViewToolbar.vue";
