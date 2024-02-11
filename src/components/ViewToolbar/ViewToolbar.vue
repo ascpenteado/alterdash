f5da9cabc22abe5f4a619189ce34ad086a96cad0
Arthur Penteado <ascpenteado@gmail.com>
2024-02-10 22:59:10 -0300

    feature: add ViewToolbar component

---
+++ src/components/ViewToolbar/ViewToolbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-toolbar dense height="60px" class="elevation-0 px-1 mb-2 rounded">
+    <v-btn v-if="showGoBack" icon @click="$router.go(-1)">
+      <v-icon color="acce">mdi-arrow-left</v-icon>
+    </v-btn>
+    <v-toolbar-title>{{ title }}</v-toolbar-title>
+    <v-spacer></v-spacer>
+
+    <v-btn v-if="addUrl" text color="accent" :to="addUrl">
+      <v-icon>mdi-plus</v-icon> Adicionar
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+const ViewToolbar = Vue.extend({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    addUrl: {
+      type: String,
+    },
+    showGoBack: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
+
+export default ViewToolbar;
+</script>
