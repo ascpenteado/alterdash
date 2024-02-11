95b314a2903475121cc564c9771c5d8cf457ee63
Arthur Penteado <ascpenteado@gmail.com>
2024-02-11 12:20:12 -0300

    refactor: update DefaultLayout.vue to import and use components

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -10,8 +10,16 @@
   </v-app>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts">
 import NavBar from "../components/NavBar/NavBar.vue";
 import NavDrawer from "../components/NavDrawer/NavDrawer.vue";
 import SnackBar from "../components/SnackBar/SnackBar.vue";
+
+export default {
+  components: {
+    NavBar,
+    NavDrawer,
+    SnackBar,
+  },
+};
 </script>
