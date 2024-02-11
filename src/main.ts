19ede108398dfe5b0bd31685e97c546594789d9d
Arthur Penteado <ascpenteado@gmail.com>
2024-02-11 19:06:54 -0300

    feat: add Maska library for form input masking

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import { vMaska } from "maska";
 
 Vue.config.productionTip = false;
+
+Vue.directive("maska", vMaska);
 
 new Vue({
   router,
