438cc82d6354b7db625f3ccd7b08ce1c442f7447
Arthur Penteado <ascpenteado@gmail.com>
2024-02-11 19:07:46 -0300

    feat: add email and phone form validation rules

--- src/utils/formValidationRules.ts
+++ src/utils/formValidationRules.ts
@@ -1,19 +1,29 @@
 export const formValidationRules = {
+  email: (value: string) => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailRegex.test(value) || "E-mail inválido";
+  },
+  phone: (value: string) => {
+    const phoneRegex = /^\(?\d{2}\)?[\s-]?\d{4,5}-?\d{4}$/;
+    return phoneRegex.test(value) || "Telefone inválido";
+  },
   required: (value: string) => !!value || "Campo obrigatório",
+  shouldBePositiveInteger: (value: string) =>
+    parseInt(value) > 0 || "O valor deve ser um número positivo",
   shouldBeNumber: (value: string) =>
-    !isNaN(parseFloat(value)) || "Valor deve ser numérico",
+    !isNaN(parseFloat(value)) || "O valor deve ser numérico",
   positivePrice: (value: string) =>
-    parseFloat(value) > 0 || "Valor deve ser positivo",
+    parseFloat(value) > 0 || "O valor deve ser positivo",
   minLength: (value: string) => value?.length >= 3 || "Mínimo de 3 caracteres",
   maxLength: (value: string) =>
     value?.length <= 255 || "Máximo de 255 caracteres",
   cpfOrCnpj: (value: string) => {
-    if (value.length === 11) {
-      return true;
-    }
-    if (value.length === 14) {
-      return true;
-    }
-    return "CPF ou CNPJ inválido";
+    const cpfRegex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
+
+    const cnpjRegex = /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/;
+
+    return (
+      cpfRegex.test(value) || cnpjRegex.test(value) || "CPF ou CNPJ inválido"
+    );
   },
 };
