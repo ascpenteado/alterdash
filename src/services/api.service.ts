7ad60bc1f78b0f36098979f870b6c95bddb1a39f
Arthur Penteado <ascpenteado@gmail.com>
2024-02-12 13:09:43 -0300

    fix: add missing baseURL validation in ApiClient constructor

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -4,6 +4,8 @@ export class ApiClient {
   private axios: AxiosInstance;
 
   constructor(baseURL: string, headers: Record<string, string>) {
+    if (!baseURL) throw new Error("Missing baseURL");
+
     this.axios = axios.create({
       timeout: 30000,
       headers,
@@ -15,9 +17,6 @@ export class ApiClient {
     args: RequestArgs,
     method: HttpMethod
   ): Promise<ResponseType> {
-    if (!process.env.VUE_APP_BASE_URL) {
-      return Promise.reject("Missing .env file or VUE_APP_BASE_URL field");
-    }
     const optionalConfig: AxiosRequestConfig = { ...args, method };
 
     const resp = await this.axios.request<ResponseType>(optionalConfig);
@@ -50,44 +49,8 @@ export class ApiClient {
   public delete<ResponseType>(requestArgs: RequestArgs): Promise<ResponseType> {
     return this.request<ResponseType>(requestArgs, HttpMethod.DELETE);
   }
-
-  // public post<ResponseType, PayloadType>(
-  //   endpoint: string,
-  //   data?: PayloadType
-  // ): Promise<ResponseType> {
-  //   return this.request<ResponseType>(
-  //     HttpMethod.POST,
-  //     endpoint,
-  //     undefined,
-  //     data
-  //   );
-  // }
-
-  // public put<ResponseType, PayloadType>(
-  //   endpoint: string,
-  //   data?: PayloadType
-  // ): Promise<ResponseType> {
-  //   return this.request<ResponseType>(
-  //     HttpMethod.PUT,
-  //     endpoint,
-  //     undefined,
-  //     data
-  //   );
-  // }
-
-  // public delete<ResponseType>(
-  //   endpoint: string,
-  //   params?: unknown
-  // ): Promise<ResponseType> {
-  //   const config: AxiosRequestConfig = {
-  //     params: params,
-  //   };
-  //   return this.request<ResponseType>(HttpMethod.DELETE, endpoint, undefined, {
-  //     params: config.params,
-  //   });
-  // }
 }
 
-export const apiClient = new ApiClient(process.env.VUE_APP_BASE_URL, {
+export const apiClient = new ApiClient(process.env.VUE_APP_API_BASE_URL, {
   "Content-Type": "application/json",
 });
