c5898c1a9595eb61eac6a123285a57c9347152d4
Arthur Penteado <ascpenteado@gmail.com>
2024-02-12 13:54:52 -0300

    refactor: update logout functionality

--- src/services/logout.ts
+++ src/services/logout.ts
@@ -1,8 +1,8 @@
-import router from "../router";
 import { removeToken } from "../utils/manageToken";
 
 export const logout = async () => {
   removeToken();
   localStorage.removeItem("id");
-  router.push("/login");
+  localStorage.removeItem("theme");
+  window.location.href = "/login";
 };
