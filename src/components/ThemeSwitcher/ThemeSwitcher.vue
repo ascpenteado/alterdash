6b1839b8bf2ea9274ef27db4d5f89e901d04a276
Arthur Penteado <ascpenteado@gmail.com>
2024-02-12 13:49:35 -0300

    refactor: theme switcher component and add local storage support

--- src/components/ThemeSwitcher/ThemeSwitcher.vue
+++ src/components/ThemeSwitcher/ThemeSwitcher.vue
@@ -1,9 +1,15 @@
 <template>
   <v-select
-    :items="['light', 'dark', 'system']"
+    v-model="select"
+    :items="items"
+    item-text="text"
+    item-value="value"
+    label="Selecione o tema"
+    persistent-hint
+    return-object
+    single-line
     color="accent"
-    v-model="theme"
-    label="Tema"
+    item-color="accent"
   ></v-select>
 </template>
 
@@ -11,22 +17,45 @@
 import { isDarkModePreferredByUser } from "@/utils/isDarkModePreferredByUser";
 import Vue from "vue";
 
+const themes = [
+  {
+    value: "light",
+    text: "Claro",
+  },
+  {
+    value: "dark",
+    text: "Escuro",
+  },
+  {
+    value: "system",
+    text: "Preferência do Sistema",
+  },
+];
+
+const currentThemePreference = localStorage.getItem("theme");
+
 const ThemeSwitcher = Vue.extend({
   data() {
     return {
-      theme: "system",
+      items: themes,
+      select: currentThemePreference
+        ? { value: currentThemePreference }
+        : themes[2],
     };
   },
   watch: {
-    theme(theme) {
-      switch (theme) {
+    select(theme) {
+      switch (theme.value) {
         case "light":
           this.$vuetify.theme.dark = false;
+          localStorage.setItem("theme", "light");
           break;
         case "dark":
           this.$vuetify.theme.dark = true;
+          localStorage.setItem("theme", "dark");
           break;
         case "system":
+          localStorage.removeItem("theme");
           this.$vuetify.theme.dark = isDarkModePreferredByUser();
           break;
       }
--- src/utils/isDarkModePreferredByUser.ts
+++ src/utils/isDarkModePreferredByUser.ts
@@ -1,6 +1,11 @@
 export function isDarkModePreferredByUser() {
   if (typeof window !== "undefined" && window.matchMedia) {
-    return window.matchMedia("(prefers-color-scheme: dark)").matches;
+    const localStorageTheme = localStorage.getItem("theme");
+    if (!localStorageTheme)
+      return window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    if (localStorageTheme === "dark") return true;
+    if (localStorageTheme === "light") return false;
   }
   return false;
 }
